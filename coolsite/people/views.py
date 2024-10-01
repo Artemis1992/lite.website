95f9e6e5e512b2aeeb8437918a8729c5d73f1ef5
Artem Cvetkov <Cvetkoff_1.92@mail.ru>
2024-10-01 20:56:49 +0500

    Продолжение темы миграции. Обработка исключений при запросах к серверу, обработка GET, POST запросов, 301-302 редакторы.

--- coolsite/people/views.py
+++ coolsite/people/views.py
@@ -1,15 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import redirect, render
 
 def index(request):
     return HttpResponse("Страница приложения people")
 
+
 def categories(request, catid):
+    if(request.GET):
+        print(request.GET)  # Выводим параметры в терминал
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
-# def artist(request):
-#     return HttpResponse("<h2>Актрисы</h2>")
+def archive(request, year):
+    if int(year) > 2024:
+        return redirect('home', permanent=True) # Редирект
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
-
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
 
