ad7bfef42ff51c00d4570bf376ffdbf21b127bae
Artem Cvetkov <Cvetkoff_1.92@mail.ru>
2024-10-30 22:38:57 +0500

    Сохраняем шаблоны

--- coolsite/people/urls.py
+++ coolsite/people/urls.py
@@ -4,9 +4,9 @@ from people.views import pageNotFound
 from .views import * 
 
 urlpatterns = [
-    path('home/', index, name='home'),
-    path('cats/<int:catid>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+
 ]
 
 
--- coolsite/people/views.py
+++ coolsite/people/views.py
@@ -1,9 +1,21 @@
+# HttpResponse: возвращает простой HTTP-ответ с текстом, который может быть строкой HTML.
+# render: рендерит HTML-шаблон с возможностью передавать контекстные данные в шаблон для более сложной логики отображения.
+# redirect: выполняет перенаправление на другой URL. Можно использовать параметр permanent для указания, является ли перенаправление временным (302) или постоянным (301).
+# HttpResponseNotFound: возвращает ошибку 404 с указанным HTML-сообщением.
+
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import redirect, render
 
-def index(request):
-    return HttpResponse("Страница приложения people")
+from .models import *
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+def index(request):
+    posts = People.objects.all()
+    return render(request, 'people/index.html', {"posts": posts, "menu": menu, 'title': 'Главная сттроаница'})
+
+def about(request):
+    return render(request, 'people/about.html', {"menu": menu, 'title': 'О сайте'})
 
 def categories(request, catid):
     if(request.GET):
@@ -11,8 +23,10 @@ def categories(request, catid):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
 def archive(request, year):
+    # Если год больше 2024, выполняем редирект на главную страницу
     if int(year) > 2024:
         return redirect('home', permanent=True) # Редирект
+    # Возвращаем HttpResponse с заголовком и значением года
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 def pageNotFound(request, exception):
