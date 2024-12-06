4abba4890857d965fedd380ff72c7673765b9cba
Artem Cvetkov <Cvetkoff_1.92@mail.ru>
2024-12-06 21:29:16 +0500

    Настроили Админ панель и законспектировали все

--- coolsite/people/models.py
+++ coolsite/people/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 
 class People(models.Model):
-    title = models.CharField(max_length=255)  # Заголовок или имя (макс. 255 символов)
-    content = models.TextField(blank=True)  # Текстовое поле, может быть пустым
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")  # Путь для загрузки фото
-    time_create = models.DateTimeField(auto_now_add=True)  # Время создания записи
-    time_update = models.DateTimeField(auto_now=True)  # Время обновления записи
-    is_published = models.BooleanField(default=True)  # Статус публикации (по умолчанию — опубликовано)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)  # Связь с таблицей категорий
+    title = models.CharField(max_length=255, verbose_name='Заголовок')  # Заголовок или имя (макс. 255 символов)
+    content = models.TextField(blank=True, verbose_name='Контент')  # Текстовое поле, может быть пустым
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Фото')  # Путь для загрузки фото
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создание')  # Время создания записи
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')  # Время обновления записи
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')  # Статус публикации (по умолчанию — опубликовано)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')  # Связь с таблицей категорий
 
     def __str__(self):
         return self.title  # Возвращает название записи
@@ -18,19 +18,27 @@ class People(models.Model):
     def get_absolute_url(self):
         return reverse("post", kwargs={"post_id": self.pk})
 
-
+    class Meta:
+        verbose_name = "знаменитость"
+        verbose_name_plural = 'Знаменитости'
+        ordering = ['time_create', 'title']
+        
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)  # Название категории
-
+    name = models.CharField( # Название категории
+        max_length=100, 
+        db_index=True,
+        verbose_name='Категория',
+    )  
     def __str__(self):
         return self.name  # Для отображения названия категории в админке
     
     def get_absolute_url(self):
         return reverse("category", kwargs={"cat_id": self.pk})
 
-    
-    
-    
+    class Meta:
+        verbose_name = 'категорию' # Название модели в единственном числе.
+        verbose_name_plural = 'Категории' # Название модели во множественном числе.
+        ordering = ['id'] # Сортировка записей по ID.
     
     
     
