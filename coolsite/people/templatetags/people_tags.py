4fedec286f0439cc516473f25cfdef26c7450578
Artem Cvetkov <Cvetkoff_1.92@mail.ru>
2024-12-18 18:10:29 +0500

    11. Пользовательские теги шаблонов

--- coolsite/people/templatetags/people_tags.py
+++ coolsite/people/templatetags/people_tags.py
@@ -1,49 +1,26 @@
 from django import template
 from people.models import *
-from people.views import menu # из views.py
-from people.views import show_category, index
+from people.views import menu, show_post
 
 register = template.Library()
 
 
 
-@register.simple_tag(name="getcats") # Регистрация простого тега для получения категорий
-def get_categories(filter=None):
-    if not filter:
-        return Category.objects.all()
-    else:
-        return Category.objects.filter(pk=filter)
+@register.simple_tag(name="getcats") # мы изменили имя с get_categories на getcats.
+def get_categories():
+    return Category.objects.all()
 
 
-@register.inclusion_tag("people/list_categories.html")
-def show_categories(sort=None, cat_selected=0):
-    """
-    Включающий тег для отображения списка категорий и меню.
-    
-    :param sort: Параметр для сортировки категорий.
-    :param cat_selected: ID выбранной категории.
-    :return: Словарь с данными для рендеринга шаблона.
-    """
-    if not sort:
-        cats = Category.objects.all()
-    else:
-        cats = Category.objects.order_by(sort)
-        
-    return {
-        "cats": cats, 
-        "cat_selected": cat_selected,
-    }
+
+@register.inclusion_tag('people/list_categories.html')
+def show_categories():
+    cats = Category.objects.all()
+    return {'cats': cats}
 
 
-@register.inclusion_tag("people/list_menu.html")
+@register.inclusion_tag('people/list_menu.html')
 def show_menu():
-        return {
-            "menu": menu,
-        }
-
-
-
-
-
-
+    return {
+        'menu': menu,
+    }
 
--- coolsite/people/views.py
+++ coolsite/people/views.py
@@ -14,17 +14,9 @@ menu = [{"title": "О сайте", "url_name": "about"},
         {"title": "Войти", "url_name": "login"},
 ]
 
-
-
 def index(request):
-    """
-    Эта функция обрабатывает запрос для главной страницы приложения.
-    Она выбирает все записи из модели People и все категории из модели Category.
-    Контекст, содержащий список записей, категории, меню, заголовок страницы и выделенную категорию (0 — для всех категорий),
-    передаётся в шаблон 'people/index.html' для отображения содержимого главной страницы.
-    """
     posts = People.objects.all()
-    
+
     context = {
         'posts': posts,        # Список записей
         'title': 'Главная страница',  # Заголовок страницы
@@ -34,22 +26,24 @@ def index(request):
     return render(request, 'people/index.html', context=context)
 
 
-
 def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+    post = get_object_or_404(People, pk=post_id) 
+    
+    context = {
+        'post': post,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
 
+    return render(request, 'people/post.html', context=context)
+    
+    
+    # return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 
 def show_category(request, cat_id):
-    """
-    Эта функция обрабатывает запрос для отображения записей, относящихся к определённой категории (cat_id).
-    Она выбирает записи модели People, у которых поле cat_id совпадает с переданным параметром.
-    Если записи не найдены, вызывается исключение Http404.
-    Контекст с данными (записи, категории, меню, заголовок и выделенная категория) передаётся в шаблон 'people/index.html' для рендеринга.
-    """
     posts = People.objects.filter(cat_id=cat_id) 
 
-    
     if len(posts) == 0:
         raise Http404
     
@@ -59,8 +53,6 @@ def show_category(request, cat_id):
         'cat_selected': cat_id,
     }
     return render(request, 'people/index.html', context=context)
-     
-     
      
 def about(request):
     return render(request, 'people/about.html', {"menu": menu, 'title': 'О сайте'})
