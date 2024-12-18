c1215131231470ff012ad49e603cecddd781157a
Artem Cvetkov <Cvetkoff_1.92@mail.ru>
2024-12-18 18:37:01 +0500

    Дополнил и еще немного поработал с пользовательскими тегами

--- coolsite/people/templatetags/people_tags.py
+++ coolsite/people/templatetags/people_tags.py
@@ -7,15 +7,22 @@ register = template.Library()
 
 
 @register.simple_tag(name="getcats") # мы изменили имя с get_categories на getcats.
-def get_categories():
-    return Category.objects.all()
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
 
 
 
 @register.inclusion_tag('people/list_categories.html')
-def show_categories():
-    cats = Category.objects.all()
-    return {'cats': cats}
+def show_categories(sort=None, cat_selected=0):
+    if not sort:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.order_by(sort)
+    
+    return {"cats": cats, "cat_selected": cat_selected}
 
 
 @register.inclusion_tag('people/list_menu.html')
